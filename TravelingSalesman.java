2a87452e63cffa7b4f670f4c523a69718c453c7a
mamenjivar <Miguel@macs-MacBook-Pro.local>
2018-09-29 15:18:58 -0700

    first upload, this is where the project will be programmed on.

---
+++ TravelingSalesman.java
@@ -0,0 +1,22 @@
+package tsp;
+
+/**
+ * Member Names:
+ * Miguel Menjivar
+ * Richard Cota
+ * Chris Rauch
+ * 
+ * Project: Traveling Salesman
+ */
+public class TravelingSalesman {
+
+	/**
+	 * Main
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+		
+	}
+
+}
