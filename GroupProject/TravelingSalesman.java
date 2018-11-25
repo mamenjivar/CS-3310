ab11cad6928c078df2392a6d6ae861b317982c8c
mamenjivar <menjivarmiguel221@gmail.com>
2018-11-24 20:07:37 -0800

    updating project

--- GroupProject/TravelingSalesman.java
+++ GroupProject/TravelingSalesman.java
@@ -1,4 +1,9 @@
 package tsp;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Member Names: 
  * Miguel Menjivar 
@@ -10,8 +15,18 @@ package tsp;
  * Project: Traveling Salesman
  */
 public class TravelingSalesman {
-	static int[][] map = { { 0, 50, 100, 9 }, { 75, 0, 25, 15 }, { 30, 15, 0, 45 }, {25, 20, 30, 0}};
+	// global variables
+	static Scanner keyboard = new Scanner(System.in);
+
+	// example purposes fixed 2d array
+	static int[][] map = { { 0, 50, 100, 9 }, { 75, 0, 25, 15 }, { 30, 15, 0, 45 }, { 25, 20, 30, 0 } };
+	static int[][] randMap;
+	static int MAP_SIZE = map.length;
 	static int minimum = 99999;
+	// static int[] pathResult = new int[MAP_SIZE];
+	static int[] pathResult;
+
+	static ArrayList<Integer> al = new ArrayList<>();
 
 	/**
 	 * Main
@@ -19,23 +34,56 @@ public class TravelingSalesman {
 	public static void main(String[] args) {
 		System.out.println("Traveling Salesman\n");
 
-		int size = map.length;	
-		int[] permute = new int[size];
-		
+		// modify this value to get matrix size n x n
+		System.out.print("n=");
+//		int n = 4;
+		int n = keyboard.nextInt();
+		System.out.println();
+		populateArray(n);
+
+		// amount of cities on map
+//		int[] permute = new int[MAP_SIZE];
+		int[] permute = new int[n];
+
 		// populates permutations based on size of map
-		for(int i = 0; i < permute.length; i++) {
+		for (int i = 0; i < permute.length; i++) {
 			permute[i] = i;
 		}
 
 		// prints out the 2d array map
-		System.out.println("Map");
-		printMap(map, size);
+		System.out.println("Map:");
+//		printMap(map, MAP_SIZE);
+		printMap(randMap, n);
 
+		System.out.println("Paths:");
 		// performs permutations and calculates distances from all cities
 		permutations(permute, permute.length, permute.length);
 		System.out.println();
 
-		System.out.println("minimum path: " + minimum);
+		// prints out minimum distance to other cities
+		System.out.println("minimum path:");
+		printArray(pathResult);
+		System.out.println(" = " + minimum);
+		
+		keyboard.close();
+
+	}
+
+	/**
+	 * randomize a random array
+	 */
+	static void populateArray(int matrixSize) {
+		Random rand = new Random();
+		randMap = new int[matrixSize][matrixSize];
+		for (int i = 0; i < matrixSize; i++) {
+			for (int j = 0; j < matrixSize; j++) {
+				if (i == j) {
+					randMap[i][j] = 0;
+				} else {
+					randMap[i][j] = rand.nextInt(100);
+				}
+			}
+		}
 	}
 
 	/**
@@ -48,10 +96,12 @@ public class TravelingSalesman {
 	static void permutations(int a[], int size, int n) {
 
 		if (size == 1) {
-			printArray(a);
-			bruteForcetsp(map, a);
+//			printArray(a);
+//			bruteForcetsp(map, a);
+			bruteForcetsp(randMap, a);
 		}
 
+		// research this
 		for (int i = 0; i < size; i++) {
 			permutations(a, size - 1, n);
 
@@ -65,27 +115,48 @@ public class TravelingSalesman {
 			}
 		}
 	}
-	
+
 	/**
 	 * traveling salesman problem brute force
 	 */
 	static void bruteForcetsp(int[][] map, int[] a) {
 		int sum = 0;
 		for (int i = 0; i < a.length - 1; i++) {
-			sum += map[a[i]][a[i + 1]];
+			 sum += map[a[i]][a[i + 1]];
+//			sum += randMap[a[i]][a[i + 1]];
 		}
 		sum += map[a[a.length - 1]][a[0]];
+//		sum += randMap[a[a.length - 1]][a[0]];
 
-		System.out.print(" = " + sum + "\n");
-		minimum = min(minimum, sum);
+//		System.out.print(" = " + sum + "\n");
+		minimum = min(minimum, sum, a);
+
+//		if (duplicates(sum)) {
+			printArray(a);
+			System.out.print(" = " + sum + "\n");
+//		}
+
+	}
+
+	/**
+	 * 
+	 * @param sum
+	 */
+	static boolean duplicates(int sum) {
+		if(al.contains(sum)) {
+			return false;
+		} else {
+			al.add(sum);
+			return true;
+		}
 	}
 
 	/**
 	 * swaps the values in permutation array
 	 * 
 	 * @param array
-	 * @param j 1st value to be swapped
-	 * @param k 2nd value to be swapped
+	 * @param j     1st value to be swapped
+	 * @param k     2nd value to be swapped
 	 */
 	static void swap(int[] array, int j, int k) {
 		int temp = array[j];
@@ -94,7 +165,7 @@ public class TravelingSalesman {
 	}
 
 	/**
-	 * prints each path 
+	 * prints each path
 	 * 
 	 * @param a
 	 */
@@ -106,26 +177,38 @@ public class TravelingSalesman {
 	}
 
 	/**
-	 * will return the smallest value
+	 * will return the smallest value of two inputs
 	 * 
 	 * @param minPath
 	 * @param currentWeight
 	 * @return
 	 */
-	static int min(int minPath, int currentWeight) {
+	static int min(int minPath, int currentWeight, int[] array) {
 		int minimum = 0;
 		if (minPath < currentWeight) {
 			minimum = minPath;
 		} else {
 			minimum = currentWeight;
+			copyArray(array);
 		}
-
 		return minimum;
-
 	}
 
 	/**
-	 * prints out map contents neatly
+	 * copy contents of array as result to pathResult array it will hold path to
+	 * shortest distance amongst all cities back to origin
+	 * 
+	 * @param array
+	 */
+	static void copyArray(int[] array) {
+		pathResult = new int[array.length];
+		for (int i = 0; i < array.length; i++) {
+			pathResult[i] = array[i];
+		}
+	}
+
+	/**
+	 * prints out map contents of 2D array
 	 * 
 	 * @param map  holds contents of 2d array
 	 * @param size the size of array which is size x size
@@ -137,15 +220,15 @@ public class TravelingSalesman {
 			System.out.printf("%-4d", i);
 		}
 		System.out.println();
-
+ 
 		for (int i = 0; i < size; i++) {
 			System.out.printf("%-4d", i);
 			for (int j = 0; j < map[i].length; j++) {
 				System.out.printf("%-4d", map[i][j]);
+
 			}
 			System.out.println();
 		}
 		System.out.println();
 	}
-
 }
